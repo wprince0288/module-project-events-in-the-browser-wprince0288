d533c4ed90e1a2f46ecbb963e34be332b0306fd0
wprince0288 <wprince0288@gmail.com>
2024-05-25 19:04:19 -0500

    Task 2

--- frontend/index.js
+++ frontend/index.js
@@ -37,6 +37,10 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains('targeted')) {
+          document.querySelector('.targeted').classList.remove('targeted')
+          square.classList.add('targeted')
+        }
       })
     }
   }
