5833513a01ff4d3700dbfa43413ddc77bf7007a7
wprince0288 <wprince0288@gmail.com>
2024-05-25 21:48:44 -0500

    Task 3

--- frontend/index.js
+++ frontend/index.js
@@ -69,6 +69,37 @@ function moduleProject2() {
 
   document.addEventListener('keydown', evt => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    let isUp = evt.key === keys.up
+    let isDown = evt.key === keys.down
+    let isLeft = evt.key === keys.left
+    let isRight = evt.key === keys.right
+
+    let targeted = document.querySelector('.targeted')
+ 
+    if (isUp) {
+      if (targeted.parentElement.previousElementSibling) {
+        let idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+        targeted.classList.remove('targeted')
+        targeted.parentElement.previousElementSibling.children[idx].classList.add('targeted')
+      }
+    } else if (isDown) {
+      if (targeted.parentElement.nextElementSibling) {
+        let idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+        targeted.classList.remove('targeted')
+        targeted.parentElement.nextElementSibling.children[idx].classList.add('targeted')
+      }
+    }else if (isLeft) {
+        if (targeted.previousElementSibling) {
+          targeted.classList.remove('targeted')
+          targeted.previousElementSibling.classList.add('targeted')
+        }
+      } else if (isRight) {
+        if (targeted.nextElementSibling) {
+          targeted.classList.remove('targeted')
+          targeted.nextElementSibling.classList.add('targeted')
+        }
+      }
+    
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
